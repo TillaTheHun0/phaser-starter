a0b48fb89242537d08bc4e715967dd769a8e65ff
Tyler Hall <tyler@jyve.io>
2018-01-15 01:09:35 -0500

    Move examples to example directory. Add .gitkeep files to empty directories to retain project structure

--- example/main.js
+++ example/main.js
@@ -2,6 +2,10 @@ import 'pixi'
 import 'p2'
 import Phaser from 'phaser'
 
+import BootState from './states/Boot'
+import SplashState from './states/Splash'
+import GameState from './states/Game'
+
 import config from './config'
 
 class Game extends Phaser.Game {
@@ -12,9 +16,13 @@ class Game extends Phaser.Game {
 
     super(width, height, Phaser.CANVAS, 'content', null)
 
+    this.state.add('Boot', BootState, false)
+    this.state.add('Splash', SplashState, false)
+    this.state.add('Game', GameState, false)
+
     // with Cordova with need to wait that the device is ready so we will call the Boot state in another file
     if (!window.cordova) {
-      // this.state.start('Boot')
+      this.state.start('Boot')
     }
   }
 }
@@ -37,7 +45,7 @@ if (window.cordova) {
       this.receivedEvent('deviceready')
 
       // When the device is ready, start Phaser Boot state.
-      // window.game.state.start('Boot')
+      window.game.state.start('Boot')
     },
 
     receivedEvent: function (id) {
