1a37740fe2eb830e94b47edd6774e4cda14a3b3a
Joel Price <joel.m.pri@gmail.com>
2018-01-23 20:38:36 -0500

    Auto stash before rebase of "development"

---
+++ src/states/Splash.js
@@ -0,0 +1,28 @@
+import Phaser from 'phaser'
+import { centerGameObjects } from '../utils'
+
+export default class extends Phaser.State {
+  init () {}
+
+  preload () {
+    this.loaderBg = this.add.sprite(this.game.world.centerX, this.game.world.centerY, 'loaderBg')
+    this.loaderBar = this.add.sprite(this.game.world.centerX, this.game.world.centerY, 'loaderBar')
+    centerGameObjects([this.loaderBg, this.loaderBar])
+
+    this.load.setPreloadSprite(this.loaderBar)
+    //
+    // load your assets
+    //
+    this.load.image('mushroom', 'assets/images/mushroom2.png')
+
+    this.load.spritesheet('player', 'assets/images/player.png', 32, 32, 6);
+
+    this.load.image('smallBuilding', 'assets/images/building-small-placeholder.png');
+    this.load.image('bigBuilding', 'assets/images/building-big-placeholder.png');
+    this.load.image('background', 'assets/images/background-placeholder.png');
+  }
+
+  create () {
+    this.state.start('Game')
+  }
+}
